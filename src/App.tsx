4fe7e4250a24792df6a3bcfe7017aac085fa9e8a
Elena Karsten <e.karstenzv@gmail.com>
2023-09-14 16:43:50 +0800

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,32 +8,35 @@ interface ColorConverterProps {
 const ColorConverter: React.FC<ColorConverterProps> = ({ backgroundColor }) => {
   const [hexColor, setHexColor] = useState<string>('');
   const [rgbColor, setRgbColor] = useState<string | null>(null);
+  const [hasError, setHasError] = useState<boolean>(false);
 
   const handleHexChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = event.target.value;
     setHexColor(inputValue);
-//checking by test()
+    //checking by test()
     if (/^#[0-9A-Fa-f]{6}$/.test(inputValue)) {
       const r = parseInt(inputValue.slice(1, 3), 16);
       const g = parseInt(inputValue.slice(3, 5), 16);
       const b = parseInt(inputValue.slice(5, 7), 16);
       setRgbColor(`rgb(${r}, ${g}, ${b})`);
+      setHasError(false);
     } else {
       setRgbColor(null);
+      setHasError(true);
     }
-//another way of checking
-// if (inputValue.length === 7 && inputValue[0] === '#' && /^[0-9A-Fa-f]+$/.test(inputValue.slice(1))) {
-//   const r = parseInt(inputValue.slice(1, 3), 16);
-//   const g = parseInt(inputValue.slice(3, 5), 16);
-//   const b = parseInt(inputValue.slice(5, 7), 16);
-//   setRgbColor(`rgb(${r}, ${g}, ${b})`);
-// } else {
-//   setRgbColor(null);
-// }    
+    //another way of checking
+    // if (inputValue.length === 7 && inputValue[0] === '#' && /^[0-9A-Fa-f]+$/.test(inputValue.slice(1))) {
+    //   const r = parseInt(inputValue.slice(1, 3), 16);
+    //   const g = parseInt(inputValue.slice(3, 5), 16);
+    //   const b = parseInt(inputValue.slice(5, 7), 16);
+    //   setRgbColor(`rgb(${r}, ${g}, ${b})`);
+    // } else {
+    //   setRgbColor(null);
+    // }    
   };
 
   const backgroundColorStyle = {
-    backgroundColor: rgbColor !== null ? rgbColor : backgroundColor,
+    backgroundColor: hasError ? 'red' : rgbColor !== null ? rgbColor : backgroundColor,
   };
 
   return (
